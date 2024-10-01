a53bfe558681e67041ae38f550f222a806af83da
MaxBoving <146762130+MaxBoving@users.noreply.github.com>
2024-09-30 22:56:22 -0700

    initial swift commit

---
+++ swiftcode/eatRight/eatRight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eatRight
+//
+//  Created by Max Boving on 9/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftcode/eatRight/eatRightTests/eatRightTests.swift
@@ -0,0 +1,16 @@
+//
+//  eatRightTests.swift
+//  eatRightTests
+//
+//  Created by Max Boving on 9/30/24.
+//
+
+import Testing
+
+struct eatRightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
