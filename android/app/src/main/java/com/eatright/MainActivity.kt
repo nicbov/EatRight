5f996095a3c674e18ab77f05f9b63c532ce2b8cf
Sebastien Boving <github@sebastienboving.com>
2024-11-01 15:42:45 -0700

    [Android] Startup screen.
    The logo is copied from the jpeg file from the swift codebase, only updated to make the background transparant (otherwise dark mode in Android didn't look right).
    
    Some .idea files changes included - assumed side-effects from first builds.

--- android/app/src/main/java/com/eatright/MainActivity.kt
+++ android/app/src/main/java/com/eatright/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.eatright.composables.Startup
 import com.eatright.ui.theme.EatRightTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +35,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             EatRightTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Startup(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EatRightTheme {
-        Greeting("Android")
     }
 }
---
+++ android/app/src/main/java/com/eatright/composables/Startup.kt
@@ -0,0 +1,56 @@
+package com.eatright.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.eatright.R
+import com.eatright.ui.theme.EatRightTheme
+
+
+@Composable
+fun Startup(modifier: Modifier = Modifier) { Column(
+    modifier = modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center,
+) {
+    Image(
+        painter = painterResource(id = R.drawable.eatright_logo),
+        contentDescription = stringResource(id = R.string.eatright_logo_desc),
+        modifier
+            .width(250.dp)
+            .height(250.dp),
+    )
+    CircularProgressIndicator(
+        color = Color.Green,
+        strokeWidth = 8.dp,
+        modifier = modifier,
+    )
+    Text(
+        text = "CopyRight 2024",
+        textAlign = TextAlign.Center,
+        modifier = modifier,
+    )
+}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StartupPreview() {
+    EatRightTheme {
+        Startup()
+    }
+}
